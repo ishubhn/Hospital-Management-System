fda4dc5c2f2420f407fcc4f9c24f484c63caaa91
Shubham Nagre <shubhamnagre@gmail.com>
2023-06-28 01:12:19 +0530

    [ADD] added exception handler

--- auth-service/src/main/java/io/service/auth/exception/AuthExceptionHandler.java
+++ auth-service/src/main/java/io/service/auth/exception/AuthExceptionHandler.java
@@ -3,18 +3,25 @@ package io.service.auth.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
 public class AuthExceptionHandler extends ResponseEntityExceptionHandler {
-    public ResponseEntity<ErrorInfo> handleUserAlreadyExistException(UserAlreadyExistException e) {
-        return new ResponseEntity<>(new ErrorInfo("ILLEGAL_ARGUMENT", "Username already exists"),
-                HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(UserAlreadyExistException.class)
+    public ResponseEntity<ErrorInfo> handleUserAlreadyExistException(UserAlreadyExistException ex, WebRequest request) {
+        ErrorInfo errorInfo = new ErrorInfo(ex.getMessage(),
+                request.getDescription(true));
+
+        return new ResponseEntity<>(errorInfo, HttpStatus.BAD_REQUEST);
     }
 
-    public ResponseEntity<ErrorInfo> handleBadCredentialsException(BadCredentialsException ex) {
-        return new ResponseEntity<>(new ErrorInfo("BAD_CREDENTIALS", "Invalid Username or Password"),
-                HttpStatus.UNAUTHORIZED);
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorInfo> handleBadCredentialsException(BadCredentialsException ex, WebRequest request) {
+        ErrorInfo errorInfo = new ErrorInfo(ex.getMessage(),
+                request.getDescription(true));
+        return new ResponseEntity<>(errorInfo, HttpStatus.UNAUTHORIZED);
     }
 }
