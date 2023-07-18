3d4d3467e56c24ab084cd2e41a66fbe51dcce9b6
Shubham Nagre <imshubhn@gmail.com>
2023-07-18 19:47:48 +0530

    [NEW] .

--- api-service/src/main/java/io/management/gateway/api/model/dto/AuthResponse.java
+++ api-service/src/main/java/io/management/gateway/api/model/dto/AuthResponse.java
@@ -1,4 +1,18 @@
 package io.management.gateway.api.model.dto;
 
+import lombok.*;
+
+import java.util.Collection;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponse {
+	private String userId;
+	private String accessToken;
+	private String refreshToken;
+	private long expireAt;
+	private Collection<String> authorities;
 }
